09d899fc05503b7169cfcb14c3b49519e6704f0e
santiagoC <s.calderon@cgiar.org>
2025-10-09 11:20:12 -0500

    Refactor project dependencies and enhance CSV data handling
    - Updated `pyproject.toml` and `requirements.txt` to include additional dependencies for CSV processing and data validation.
    - Introduced `CSVClient` class for extracting climate data from CSV files, including methods for reading, validating, and processing data.
    - Modified `aclimate_run_etl.py` to support data extraction from both GeoServer and CSV sources, with appropriate argument parsing.
    - Enhanced logging and error handling throughout the CSV data extraction process.
    - Updated `DatabaseManager` to reflect changes in measurement mapping for solar radiation.

--- src/aclimate_v3_historical_location_etl/data_managment/__init__.py
+++ src/aclimate_v3_historical_location_etl/data_managment/__init__.py
@@ -1,8 +1,10 @@
+from .csv_client import CSVClient
 from .data_validator import DataValidator
 from .database_manager import DatabaseManager
 from .geoserver_client import GeoServerClient
 
 __all__ = [
+    "CSVClient",
     "DataValidator",
     "DatabaseManager",
     "GeoServerClient",
